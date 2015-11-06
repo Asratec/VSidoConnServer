08c4ab20b76a06d6a606be7d8d1872534cc1693f
Ma Eikei <yma@asratec.co.jp>
2015-11-06 16:52:25 +0900

    change directory structure

--- VSidoWeb/debug/WebServer.js
+++ VSidoWeb/debug/WebServer.js
@@ -2,7 +2,7 @@ var http = require("http"),
     url = require("url"),
     path = require("path"),
     fs = require("fs"),
-    port = process.argv[2] || 8088;
+    port = process.argv[2] || 18089;
  
      
 http.createServer(function(request, response) {
--- VSidoWeb/web.server.new.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /home/sysroot/usr/share/WebServer && node WebServer.js
-
